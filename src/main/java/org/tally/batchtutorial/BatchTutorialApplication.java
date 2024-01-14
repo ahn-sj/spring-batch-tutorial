6933b7d08a44f5365b0dc3549defde7b43c04021
ahn-sj <jaesa5221@gmail.com>
2024-01-14 13:37:51 +0900

    Initial commit

---
+++ src/main/java/org/tally/batchtutorial/BatchTutorialApplication.java
@@ -0,0 +1,13 @@
+package org.tally.batchtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchTutorialApplication.class, args);
+    }
+
+}
