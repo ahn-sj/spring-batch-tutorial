389707e44af34d27b308ef7dff208d24a88b0c6d
ahn-sj <jaesa5221@gmail.com>
2024-01-17 22:08:31 +0900

    feat: schedule and batch job 추가

--- src/main/java/org/tally/batchtutorial/BatchTutorialApplication.java
+++ src/main/java/org/tally/batchtutorial/BatchTutorialApplication.java
@@ -2,7 +2,9 @@ package org.tally.batchtutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BatchTutorialApplication {
 
